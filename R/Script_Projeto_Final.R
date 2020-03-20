aadef93ad339f7bcf2881d319028a8d77d51cbf8
Natalia.Souto <souto.nm@gmail.com>
2020-03-20 18:39:05 -0300

    Script final dia 1

--- R/Script_Projeto_Final.R
+++ R/Script_Projeto_Final.R
@@ -3,6 +3,40 @@
 
 library("tidyr")
 
-# Lendo a planilha de dados
+# Lendo a planilha de dados #
 read.csv2("Dados/Planilha_R_editado.csv")
+
+# Nomeando a planilha #
+Dados <- read.csv2("Dados/Planilha_R_editado.csv")
+
+# Número de linhas da planilha #
+nrow(Dados)
+
+# Entendo os dados #
+head(Dados)
+dim(Dados)
+
+#### Análise exploratória dos Dados ####
+summary(Dados)
+class(Dados)
+str(Dados)
+
+# Média escamas ventrais #
+mean(Dados$Esc_ventrais)
+
+# Média das escamas subcaudais #
+mean(Dados$Esc_subcaudais)
+
+# Boxplot das escamas ventrais, números na horizontal #
+boxplot(Dados$Esc_ventrais, las = 1)
+
+# Boxplot das escamas subcaudais, números na horizontal #
+boxplot(Dados$Esc_subcaudais, las = 1)
+
+boxplot(Esc_ventrais ~ Especie, data = Dados, las = 1)
+boxplot(Esc_subcaudais ~ Especie, data = Dados, las = 1)
+
+boxplot(Esc_ventrais ~ Sexo, data = Dados, las = 1)
+boxplot(Esc_subcaudais ~ Sexo, data = Dados, las = 1)
+
 
