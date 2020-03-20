53f24aea50883bb6998f1561f7abb8cb93f4d0b8
Natalia.Souto <souto.nm@gmail.com>
2020-03-20 18:23:14 -0300

    Primeira análise exploratória dos dados

---
+++ R/Script_Projeto_Final.R
@@ -0,0 +1,8 @@
+# Script trabalho final do curso Manipulação de dados no R
+# parte do curso Projetos de análise de dados em R
+
+library("tidyr")
+
+# Lendo a planilha de dados
+read.csv2("Dados/Planilha_R_editado.csv")
+
