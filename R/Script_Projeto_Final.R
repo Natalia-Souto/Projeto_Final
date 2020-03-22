b56c577acb04577e82bb22e832a76caa7808043d
Natalia.Souto <souto.nm@gmail.com>
2020-03-22 01:03:55 -0300

    Edição da planilha e análise exploratório

--- R/Script_Projeto_Final.R
+++ R/Script_Projeto_Final.R
@@ -4,10 +4,10 @@
 library("tidyr")
 
 # Lendo a planilha de dados #
-read.csv2("Dados/Planilha_R_editado.csv")
+read.csv2("Dados/Planilha_R_editado_3.csv")
 
 # Nomeando a planilha #
-Dados <- read.csv2("Dados/Planilha_R_editado.csv")
+Dados <- read.csv2("Dados/Planilha_R_editado_3.csv")
 
 # Número de linhas da planilha #
 nrow(Dados)
@@ -27,16 +27,48 @@ mean(Dados$Esc_ventrais)
 # Média das escamas subcaudais #
 mean(Dados$Esc_subcaudais)
 
+# Média CRC
+mean(Dados$CRC)
+
+# Média CC
+mean(Dados$CC)
+
 # Boxplot das escamas ventrais, números na horizontal #
 boxplot(Dados$Esc_ventrais, las = 1)
 
 # Boxplot das escamas subcaudais, números na horizontal #
 boxplot(Dados$Esc_subcaudais, las = 1)
 
+# Boxplot das escamas ventrais e subcaudais por espécie
 boxplot(Esc_ventrais ~ Especie, data = Dados, las = 1)
 boxplot(Esc_subcaudais ~ Especie, data = Dados, las = 1)
 
+# Boxplot das escamas ventrais e subcaudais por sexo
 boxplot(Esc_ventrais ~ Sexo, data = Dados, las = 1)
 boxplot(Esc_subcaudais ~ Sexo, data = Dados, las = 1)
 
+# Boxplot do comprimento CRC e CC por espécie
+boxplot(CRC ~ Especie, data = Dados, las = 1)
+boxplot(CC ~ Especie, data = Dados, las = 1)
 
+# Boxplot do comprimento CRC e CC por sexo
+boxplot(CRC ~ Sexo, data = Dados, las = 1)
+boxplot(CC ~ Sexo, data = Dados, las = 1)
+
+
+par(mfrow = c(2,2))
+hist(Dados$Esc_ventrais, las = 1)
+hist(Dados$Esc_subcaudais, las = 1)
+hist(Dados$CRC, las = 1)
+hist(Dados$CC, las = 1)
+
+
+par(mfrow = c(1, 1))
+hist(Dados$Esc_ventrais, las = 1, breaks = 4)
+par(mfrow = c(1, 1))
+par(mfrow = c(1, 2))
+
+
+plot(density(Dados$Esc_ventrais))
+hist(Dados$Esc_ventrais, freq = FALSE)
+lines(density(Dados$Esc_ventrais), col = "blue")
