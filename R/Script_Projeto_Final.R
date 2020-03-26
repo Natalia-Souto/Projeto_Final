72dc696fab5526ea408f82522a55b3c9e501c6b0
Natalia.Souto <souto.nm@gmail.com>
2020-03-26 19:14:29 -0300

    Modificação graficos boxplot no script

--- R/Script_Projeto_Final.R
+++ R/Script_Projeto_Final.R
@@ -11,12 +11,17 @@ read.csv2("Dados/Planilha_R_editado_4.csv")
 
 # Nomeando a planilha #
 Dados <- read.csv2("Dados/Planilha_R_editado_4.csv")
+Liophis_dorsalis <- Dados[1:63, 1:6]
+Liophis_jaegeri <- Dados[64: 116, 1:6]
 
 # Número de linhas da planilha #
 nrow(Dados)
 
 # Entendo os dados #
+Liophis_dorsalis
+Liophis_jaegeri
 head(Dados)
+head(Liophis_dorsalis)
 dim(Dados)
 
 #### Análise exploratória dos Dados ####
@@ -47,31 +52,45 @@ par(mfrow = c(1,2))
 boxplot(Esc_ventrais ~ Especie, data = Dados, las = 1)
 boxplot(Esc_subcaudais ~ Especie, data = Dados, las = 1)
 
-# Boxplot das escamas ventrais e subcaudais por sexo
-par(mfrow = c(1,2))
-boxplot(Esc_ventrais ~ Sexo, data = Dados, las = 1)
-boxplot(Esc_subcaudais ~ Sexo, data = Dados, las = 1)
+# Boxplot das escamas ventrais e subcaudais por sexo e espécie
+par(mfrow = c(2,2))
+boxplot(Esc_ventrais ~ Sexo, data = Liophis_dorsalis, las = 1)
+boxplot(Esc_subcaudais ~ Sexo, data = Liophis_dorsalis, las = 1)
+boxplot(Esc_ventrais ~ Sexo, data = Liophis_jaegeri, las = 1)
+boxplot(Esc_subcaudais ~ Sexo, data = Liophis_jaegeri, las = 1)
 
 # Boxplot do comprimento CRC e CC por espécie
 par(mfrow = c(1,2))
 boxplot(CRC ~ Especie, data = Dados, las = 1)
 boxplot(CC ~ Especie, data = Dados, las = 1)
 
-# Boxplot do comprimento CRC e CC por sexo
-par(mfrow = c(1,2))
-boxplot(CRC ~ Sexo, data = Dados, las = 1)
-boxplot(CC ~ Sexo, data = Dados, las = 1)
+# Boxplot do comprimento CRC e CC por sexo e espécie
+par(mfrow = c(2,2))
+boxplot(CRC ~ Sexo, data = Liophis_dorsalis, las = 1)
+boxplot(CC ~ Sexo, data = Liophis_dorsalis, las = 1)
+boxplot(CRC ~ Sexo, data = Liophis_jaegeri, las = 1)
+boxplot(CC ~ Sexo, data = Liophis_jaegeri, las = 1)
 
 
-# Frequência do número de Escamas ventrais e subcaudais
+# Frequência do número de Escamas ventrais por espécie
 par(mfrow = c(1,2))
-hist(Dados$Esc_ventrais, las = 1)
-hist(Dados$Esc_subcaudais, las = 1)
+hist(Liophis_dorsalis$Esc_ventrais, las = 1)
+hist(Liophis_jaegeri$Esc_ventrais, las = 1)
 
-# Frequência do comprimento CRC e CC
+# Frequência do número de Escamas subcaudais por espécie
 par(mfrow = c(1,2))
-hist(Dados$CRC, las = 1)
-hist(Dados$CC, las = 1)
+hist(Liophis_dorsalis$Esc_subcaudais, las = 1)
+hist(Liophis_jaegeri$Esc_subcaudais, las = 1)
+
+# Frequência de CRC por espécie
+par(mfrow = c(1,2))
+hist(Liophis_dorsalis$CRC, las = 1)
+hist(Liophis_jaegeri$CRC, las = 1)
+
+# Frequência de CC por espécie
+par(mfrow = c(1, 2))
+hist(Liophis_dorsalis$CC, las = 1)
+hist(Liophis_jaegeri$CC, las = 1)
 
 
 # não sei o que significa!
